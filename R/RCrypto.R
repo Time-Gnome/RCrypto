25090d777c680db002eae20a3ed3f4abe59c5723
Time-Gnome <35777882+Time-Gnome@users.noreply.github.com>
2018-01-29 09:37:45 -0700

    Add files via upload

--- R/RCrypto.R
+++ R/RCrypto.R
@@ -1,13 +1,16 @@
 # Currencies avalible in the coinmarketcap api
 currencies_list_CoinMarketCap <- c("AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB", "TRY", "TWD", "ZAR", "USD")
 
+globalVariables("symbol")
+#symbol <- NULL
+
 # coinmarketcap resets the endpoint every 5 minutes
 # to avoid connection timeout httr was used instead of RCurl
 # JSON data extracted and conveted to UTF-8 text then set as a dataframe
 # time stamps are in Unix Time
 
 # get top 100 currencies by marketcap, default currency to USD
-CoinMarketCap_Top100 <- function(currency = 'USD') {
+CoinMarketCap_Top100 <- function(currency = "USD") {
 
   stopifnot(currency %in% currencies_list_CoinMarketCap)
 
@@ -16,7 +19,7 @@ CoinMarketCap_Top100 <- function(currency = 'USD') {
 }
 
 # get all currencies, default currency to USD
-CoinMarketCap_All <- function(currency = 'USD') {
+CoinMarketCap_All <- function(currency = "USD") {
 
   stopifnot(currency %in% currencies_list_CoinMarketCap)
 
@@ -25,7 +28,7 @@ CoinMarketCap_All <- function(currency = 'USD') {
 }
 
 # get selected currency by ticker, default currency to USD and tickr to BTC (bitcoin)
-CoinMarketCap_Ticker <- function(currency = 'USD', ticker = 'BTC') {
+CoinMarketCap_Ticker <- function(currency = "USD", ticker = "BTC") {
 
   stopifnot(currency %in% currencies_list_CoinMarketCap)
 
